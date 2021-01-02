97bf9d2e13e52729280058a66a1e4d615446b824
Aileen Gallacher <aileen.gallacher@service.nsw.gov.au>
2021-01-02 16:28:36 +1100

    Initial commit

---
+++ src/main/kotlin/com/example/demo/Application.kt
@@ -0,0 +1,50 @@
+package com.example.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.data.repository.CrudRepository
+import org.springframework.web.bind.annotation.*
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
+
+@SpringBootApplication
+class DemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemoApplication>(*args)
+}
+
+@RestController
+class ProductController (val productRepository: ProductRepository){
+
+	@PostMapping("/product")
+	fun addProduct(@RequestBody product : Product) {
+		productRepository.save(product)
+	}
+
+	@GetMapping("/product")
+	fun getAllProducts() : List<Product> {
+		return productRepository.findAll().toList()
+	}
+
+	@GetMapping("/product/{category}")
+	fun getProductByCategory(@PathVariable("category")category: String): Product {
+		return productRepository.findByCategory(category)
+	}
+}
+
+interface ProductRepository : CrudRepository<Product, Long> {
+	fun findByCategory(category: String) : Product
+}
+
+@Entity
+class Product(
+		var productName: String? = null,
+		var price: String? = null,
+		var weight: String? = null,
+		@Id
+		@GeneratedValue
+		var productId: Long,
+		var category: String? = null
+)
---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO product (productName, price, weight, category) VALUES
+    ('Weetbix', '3.00', '500g', 'cereal'),
+    ('Nutri Grain', '4.50', '500g', 'cereal'),
+    ('Salami', '26.50', '1kg', 'cold meat'),
+    ('Shampoo', '5.00', '750ml', 'toiletries');
