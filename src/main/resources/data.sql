29f109eb627064c5b46fb949ea3be9e466f38ef8
Aileen Gallacher <aileen.gallacher@service.nsw.gov.au>
2021-01-11 22:33:52 +1100

    fix order of props in data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO product VALUES (1001, 'Weetbix', '3.00', '500g', 'cereal');
-INSERT INTO product VALUES (1002, 'Nutri Grain', '4.50', '500g', 'cereal');
-INSERT INTO product VALUES (1003, 'Salami', '26.50', '1kg', 'cold meat');
-INSERT INTO product VALUES (1004, 'Shampoo', '5.00', '750ml', 'toiletries');
+INSERT INTO product VALUES (1001, 'cereal', '3.00', 'Weetbix', '500g');
+INSERT INTO product VALUES (1002, 'cereal', '4.50', 'Nutri Grain', '500g');
+INSERT INTO product VALUES (1003, 'cold meat', '26.50', 'Salami', '1kg');
+INSERT INTO product VALUES (1004, 'toiletries', '5.00', 'Shampoo', '750ml');
